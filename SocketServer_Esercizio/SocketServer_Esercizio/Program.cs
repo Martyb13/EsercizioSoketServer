c41544559997b90e169aaa55d47839471c4aa125
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-11-13 11:51:07 +0100

    Creazione di un nuovo progetto

---
+++ SocketServer_Esercizio/SocketServer_Esercizio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SocketServer_Esercizio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
